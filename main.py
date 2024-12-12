be57f06509a7805aee557c1a6bffa9971a9715bb
Mufchimaffia <mufarocchisango@gmail.com>
2024-12-12 19:56:08 +0200

    commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def arraysum(a):
+
+     
+     sum = 0
+     for i in a:
+
+       sum += i
+
+     return sum
+
+a = [12, 3, 4, 15]
+
+result = arraysum(a) # Store the result of the function
+
+print(result) # Print the result to the console
